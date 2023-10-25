366d522d31d3102f7d65678ba401bb656f9d8dae
Miguel Castillo <89051407+MiguelCastilloSanchez@users.noreply.github.com>
2023-10-25 11:14:57 -0600

    Add files via upload

---
+++ TypePerfect/src/main/java/domain/WordsReader.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Miguel
+ */
+public class WordsReader {
+    // Clase que lee los archivos con las palabras y devuelve listas con estos
+    private WordsReader(){}
+}
