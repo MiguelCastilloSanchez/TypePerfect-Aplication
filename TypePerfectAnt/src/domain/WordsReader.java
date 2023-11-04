911f964db10e4699a6f433cfd50b8722348cf954
diegoalamilla <diego.alamilla123@gmail.com>
2023-11-04 12:27:26 -0600

    Recuperar palabras de txt y txt con palabras requeridas

--- TypePerfectAnt/src/domain/WordsReader.java
+++ TypePerfectAnt/src/domain/WordsReader.java
@@ -4,11 +4,41 @@
  */
 package domain;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
 /**
  *
  * @author Miguel
  */
 public class WordsReader {
-    // Clase que lee los archivos con las palabras y devuelve listas con estos
-    private WordsReader(){}
-}
+    
+    public WordsReader(){
+        
+    }
+
+    public String[] getWords(String filePath) {
+        
+         String[] listOfWords = null;
+        try( BufferedReader fileReader = new BufferedReader(new FileReader(filePath))){
+        listOfWords = fileReader.lines().toArray(String[]::new);
+        this.randomizeWords(listOfWords);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return listOfWords;
+
+    }
+
+    private void randomizeWords(String[] listOfWords){
+          Random random = new Random();
+          for (int i = listOfWords.length - 1; i > 0; i--)
+          {
+            int index = random.nextInt(i + 1);
+            String randomWord= listOfWords[index];
+            listOfWords[index] = listOfWords[i];
+            listOfWords[i] = randomWord;
+          }
+        }
+    }
