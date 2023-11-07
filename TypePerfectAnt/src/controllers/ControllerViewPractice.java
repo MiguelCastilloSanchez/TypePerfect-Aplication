fde29a1e5ca01ccb89febcde3ff84c6f60395106
BuenfilJean <jeanbuenfil.jb@gmail.com>
2023-11-07 17:31:03 -0600

    feat: :sparkles: Registro de los inputs del usuario mediante el teclado
    Closes #RF8

--- TypePerfectAnt/src/controllers/ControllerViewPractice.java
+++ TypePerfectAnt/src/controllers/ControllerViewPractice.java
@@ -4,8 +4,13 @@
  */
 package controllers;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+import domain.KeyboardListener;
+import domain.WordsReader;
 import views.*;
 
 /**
@@ -14,26 +19,65 @@ import views.*;
  */
 public class ControllerViewPractice implements ActionListener{
     private ViewPractice viewPractice;
-    
-    public ControllerViewPractice(ViewPractice viewPractice){
+    private KeyboardListener keyboardListener;
+    private boolean wasStartPressed = false;
+    private Random random = new Random();
+    private String[] listOfWords;
+
+
+    public ControllerViewPractice(ViewPractice viewPractice, KeyboardListener keyboardListener){
        this.viewPractice = viewPractice; 
-       
+       this.keyboardListener = keyboardListener;
+
        setActionListenerToButtons();
+       setListenersToTextField();
        
        initializeView();
        
        viewPractice.setVisible(true);
+
+       setListOfWords();
     }
     
     @Override
-    public void actionPerformed(ActionEvent e){
-        
+    public void actionPerformed(ActionEvent event){
+        if(viewPractice.getButtonStart() == event.getSource()){
+            viewPractice.getButtonStop().setVisible(true);
+            viewPractice.getLabelWordBefore().setVisible(true);
+            viewPractice.getLabelUserWordBefore().setVisible(true);
+
+            wasStartPressed = true;
+            viewPractice.getFieldPractice().requestFocus();
+
+            int indexNextWord = random.nextInt(listOfWords.length);
+            viewPractice.getLabelWordActual().setText(listOfWords[indexNextWord]);
+        }
+        if(wasStartPressed){
+
+            this.viewPractice.getLabelWordActual().setVisible(true);
+            enterPerformed(event);
+        }
+        if(viewPractice.getButtonStop() == event.getSource()){
+            wasStartPressed = false;
+            viewPractice.getButtonStop().setVisible(false);
+            viewPractice.getLabelWordActual().setText("");
+
+            viewPractice.getLabelCorrectIcon().setVisible(false);
+            viewPractice.getLabelIncorrectIcon().setVisible(false);
+            viewPractice.getLabelWordBefore().setVisible(false);
+            viewPractice.getLabelUserWordBefore().setVisible(false);
+        }
     }
     
     private void setActionListenerToButtons(){
         this.viewPractice.getButtonMenu().addActionListener(this);
         this.viewPractice.getButtonStart().addActionListener(this);
         this.viewPractice.getButtonStop().addActionListener(this);
+    }
+
+    private void setListenersToTextField(){
+        this.viewPractice.getFieldPractice().addKeyListener(this.keyboardListener);
+        this.viewPractice.getFieldPractice().addActionListener(this);
     }
     
     private void initializeView(){
@@ -43,5 +87,37 @@ public class ControllerViewPractice implements ActionListener{
         this.viewPractice.getLabelUserWordBefore().hide();
         this.viewPractice.getLabelWordActual().hide();
         this.viewPractice.getButtonStop().hide();
+    }
+
+    private void enterPerformed(ActionEvent event){
+        if(this.keyboardListener.getWasEnterPressed()){
+            String wordTyped = this.viewPractice.getFieldPractice().getText();
+            String correctWord = this.viewPractice.getLabelWordActual().getText();
+
+            viewPractice.getLabelCorrectIcon().setVisible(false);
+            viewPractice.getLabelIncorrectIcon().setVisible(false);
+
+            if(wordTyped.equals(correctWord)){
+                viewPractice.getLabelCorrectIcon().setVisible(true);
+            }else{
+                viewPractice.getLabelIncorrectIcon().setVisible(true);
+            }
+           
+            viewPractice.getLabelWordBefore().setText(correctWord);
+            viewPractice.getLabelWordBefore().setVisible(true);
+
+            viewPractice.getLabelUserWordBefore().setText(wordTyped);
+            viewPractice.getLabelUserWordBefore().setVisible(true);
+
+            int indexNextWord = random.nextInt(listOfWords.length);
+            viewPractice.getLabelWordActual().setText(listOfWords[indexNextWord]);
+            viewPractice.getFieldPractice().setText("");
+            keyboardListener.resetWasEnterPressed();
+        }
+    }
+
+    private void setListOfWords(){
+        WordsReader listOfWordsToUse = new WordsReader();
+        this.listOfWords = listOfWordsToUse.getWords("TypePerfectAnt/words/BothHands.txt");
     }
 }
